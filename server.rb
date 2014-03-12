8f93e07bc7f44ca7de5bdd85c3344f348792c7a8
ewhitebloom <ewhitebloom@gmail.com>
2014-03-12 15:23:21 -0400

    spacing/indents

--- server.rb
+++ server.rb
@@ -4,11 +4,11 @@ require 'pry'
 
 get '/' do
 
-@articles = []
+  @articles = []
 
-CSV.foreach('data.csv', headers: true) do |row|
-  @articles << {title: row[0], url: row[1], description: row[2]}
-end
+  CSV.foreach('data.csv', headers: true) do |row|
+    @articles << {title: row[0], url: row[1], description: row[2]}
+  end
 
   erb :index
 end
@@ -18,16 +18,18 @@ get '/form' do
  erb :form
 end
 
+
+
 post '/article' do
 
-title = params['title']
-url = params['url']
-description = params['description']
+  title = params['title']
+  url = params['url']
+  description = params['description']
 
-File.open('data.csv', 'a', headers: true) do |file|
+  File.open('data.csv', 'a', headers: true) do |file|
     file.puts(title + ',' + url + ',' + description)
- end
+  end
 
-redirect '/'
+  redirect '/'
 
 end
